56eba2af9107b9927a15582f93fcfae1ad59eee2
YoustinaMitsho <youstinamitcho@gmail.com>
2025-06-27 17:54:11 +0300

    Host can multi host bug is fixed

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using TMPro;
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private TMP_InputField _joinCodeField;
+    [SerializeField] private Button _hostButton;
+
+    private void Start()
+    {
+        _hostButton.interactable = true;
+    }
     public async void StartHost()
     {
         NetworkManager.Singleton.ConnectionApprovalCallback = null;
         await HostSingelton.Instance.GameManager.StartHostAsync();
+        _hostButton.interactable = false;
     }
 
     public async void StartClient()
